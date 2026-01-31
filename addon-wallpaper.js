58259d42be9ac0f13ea0fd2026c7f5ea09520505
xapkss <shivji00ji@gmail.com>
2026-01-31 09:35:50 +0530

    Update wallpaper URL to a video file

--- addon-wallpaper.js
+++ addon-wallpaper.js
@@ -1,7 +1,7 @@
 // GitHub: addon-wallpaper.js
 
 // 1. Apna Pic URL yahan daalo
-const myWallpaper = "https://raw.githubusercontent.com/xapkss/ultra-smart-os/main/pic.png";
+const myWallpaper = "https://github.com/xapkss/ultra-smart-os/blame/93b7e24f1065fd0a72c3317f29f87e9234415ee0/my.mp4";
 
 // 2. System ko command do
 UltraOS.log("Addon Connected Successfully!");
